dc9bbf3529ebcfd1b352ce9752401f40debf1a8b
Bhalani-Mauli <maulibhalani13@gmail.com>
2023-11-06 20:46:51 +0100

    solved exercise

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-function fetchJokes() {
+async function fetchJokes() {
   // code here :
+  const response = await fetch(
+    "https://official-joke-api.appspot.com/random_joke"
+  );
+  const jsonResponce = await response.json();
+  return jsonResponce;
 }
 
 async function displayJokes() {
   // code here :
+
+  const jokes = await fetchJokes();
+  console.log(jokes);
+  const jokeElement = document.createElement("p");
+  jokeElement.innerHTML = jokes.setup + " " + jokes.punchline;
+  const jokeContainer = document.getElementById("joke-container");
+  jokeContainer.appendChild(jokeElement);
 }
 
 // don't forget to invoke the function :
-// displayJokes()
+displayJokes();
